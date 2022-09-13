c172e1dd4d3282251ca7ea4f629278732b1ef3b6
LucasDeAntoni <111450408+LucasDeAntoni@users.noreply.github.com>
2022-09-13 19:54:30 -0300

    Add files via upload

---
+++ furb/src/Uni4exe25.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Uni4exe25 {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+       
+        int op = tec.nextInt();
+        int n1 = tec.nextInt();
+        int n2 = tec.nextInt();
+        int resultado;
+
+        switch (op){
+            case 1:
+            resultado = n1 + n2;
+            System.out.println("resltdo: "+ resultado);
+            break;
+            case 2:
+            resultado = n1-n2;
+            System.out.println("resltado: "+ resultado);
+            break;
+            case 3:
+            
+        }
+
+
+    }
+    
+}
